5fe86994322a1fc4fc7e18a708afbba503456a47
아맞다우산 <98208452+fakerdeft@users.noreply.github.com>
2023-06-03 11:40:27 +0900

    각 함수에 주석 추가

--- src/main/java/controllers/DiceGameController.java
+++ src/main/java/controllers/DiceGameController.java
@@ -6,14 +6,14 @@ import static utils.Constant.*;
 
 public class DiceGameController {
 
-    public void run() {
+    public void run() {// 게임 실행 함수
         do {
-            tumbleDice(gameStart());
+            tumbleDice(inputCount());
         } while (!selectGoStop().equals("end"));
     }
 
 
-    public int gameStart() {
+    public int inputCount() {// 사용자가 원하는 횟수를 입력하는 함수
         String readLine;
 
         System.out.println("주사위를 굴릴 횟수를 입력하세요");
@@ -24,7 +24,7 @@ public class DiceGameController {
         return Integer.parseInt(readLine);
     }
 
-    public void tumbleDice(int readLine) {
+    public void tumbleDice(int readLine) {// 주사위 굴리는 함수
         List<Integer> tumbleList = new ArrayList<>();
         for (int i = 0; i < readLine; i++) {
             tumbleList.add(i, RANDOM.nextInt(6) + 1);
@@ -32,14 +32,14 @@ public class DiceGameController {
         System.out.println(tumbleList);
     }
 
-    public String selectGoStop() {
+    public String selectGoStop() {// 사용자가 재시작과 종료를 선택하는 함수
         String result;
         do {
             System.out.println("===============");
             System.out.println("1. 재시작 2. 종료");
             result = EXCEPTIONS.solveException2(SC.nextLine());
             System.out.println(result);
-        } while (!result.equals("1") && !result.equals("2"));
+        } while (!result.equals(result));
         if (result.equals("1")) {
             return "restart";
         }
